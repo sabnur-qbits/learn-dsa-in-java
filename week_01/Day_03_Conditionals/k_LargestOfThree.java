b4d0818fa90becba7ef320065617c0130452ab3e
Sabnur Samiha <sabnursamiha630@gmail.com>
2026-08-24 21:05:18 +0530

    day 03 one solution

--- week_01/Day_03_Conditionals/k_LargestOfThree.java
+++ week_01/Day_03_Conditionals/k_LargestOfThree.java
@@ -1,21 +1,25 @@
-public class k_LargestOfThree{
-    public static void main(String[] args){
-        int x,y,z;
-        x = 18;
-        y = 18;
-        z = 15;
-        if(x>y || z>y ){
-            //System.out.println("The biggest number is x: "+ x);
-            if(x>z){
+public class k_LargestOfThree {
+    public static void main(String[] args) {
+
+        int x = 18;
+        int y = 18;
+        int z = 15;
+
+        if (x > y) {
+
+            if (x > z) {
                 System.out.println("The biggest number is: " + x);
+            } else {
+                System.out.println("The biggest number is: " + z);
             }
-        
-            else{
-               System.out.println("The biggest number is: " + z);
+
+        } else {
+
+            if (y > z) {
+                System.out.println("The biggest number is: " + y);
+            } else {
+                System.out.println("The biggest number is: " + z);
             }
-        }
-        else{
-            System.out.println("The biggest number is: " + y);
         }
     }
- }
+}
